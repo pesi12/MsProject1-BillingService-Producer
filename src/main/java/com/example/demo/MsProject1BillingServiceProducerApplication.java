455337e5d8f05c8f9d20e4f0825b115e4856c8aa
prasa <prasa@pesi>
2023-08-04 16:40:30 +0530

    microservice

---
+++ src/main/java/com/example/demo/MsProject1BillingServiceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProject1BillingServiceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProject1BillingServiceProducerApplication.class, args);
+	}
+
+}
